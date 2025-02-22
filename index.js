c9b79650c6c8bab236c9b56288eefe4fd8d8c5b7
harshitsharmas84 <harshit.sharma.s84@kalvium.community>
2025-02-23 00:17:14 +0530

    Done

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-const express = require('express');
-const { resolve } = require('path');
+const express = require("express");
+const { resolve } = require("path");
+const mongoose = require("mongoose");
+const MenuItem = require("./models/MenuItem");
 
+mongoose
+  .connect("mongodb://localhost:27017/mydatabase")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+app.use(express.static("static"));
+
+app.post("/menu", async (req, res) => {
+  try {
+    const menuItem = new MenuItem(req.body);
+    await menuItem.save();
+    res.status(201).send(menuItem);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+app.get("/menu", async (req, res) => {
+  try {
+    const menuItems = await MenuItem.find();
+    res.send(menuItems);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
 });
 
 app.listen(port, () => {
